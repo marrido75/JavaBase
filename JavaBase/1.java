51570ca87d6523dd1fbb43767c3099318c52c1cb
marrido75 <marridohssm@hotmail.com>
2022-02-15 23:10:52 +0800

    123

---
+++ JavaBase/1.java
@@ -0,0 +1,9 @@
+/*
+ * @Author: Marrido
+ * @Date: 2022-02-15 23:10:10
+ * @LastEditTime: 2022-02-15 23:10:11
+ * @LastEditors: Marrido
+ * @Description: 
+ * @FilePath: \JavaBase\JavaBase\1.java
+ * Copyright (c) 2022 by Marrido, All Rights Reserved.
+ */
