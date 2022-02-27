16ecc0ab2de010ee9d77161be5903c24b18b96ec
marrido75 <marridohssm@hotmail.com>
2022-02-27 22:57:41 +0800

    封装end

---
+++ src/com/hspedu/encap/Account.java
@@ -0,0 +1,75 @@
+package com.hspedu.encap;
+
+
+public class Account {
+    private String name;
+    private double balance;
+    private String password;
+
+
+    public Account() {
+    }
+
+    public Account(String name, double balance, String password) {
+        setName(name);
+        setBalance(balance);
+        setPassword(password);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length() > 1 && name.length() < 5) {
+            this.name = name;
+        } else {
+            System.out.println("长度不对");
+            this.name = "沙雕";
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance > 20) {
+            this.balance = balance;
+        } else {
+            System.out.println("余额必须大于20");
+            this.balance = 8;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password.length() == 6) {
+            this.password = password;
+        } else {
+            System.out.println("密码不规范 6");
+            this.password = "123456";
+        }
+
+    }
+
+    public String Test() {
+        return "name=" + name + " balance=" + balance + " password=" + password;
+    }
+
+}
+
+class AccountTest {
+    public static void main(String[] args) {
+        //数据校验需要走构造器  或者  set方法
+        Account account = new Account("1231313132131",5,"46546546");
+        //account.setName("jack11156+545646465465");
+        //account.setBalance(19);
+        //account.setPassword("789465");
+        System.out.println(account.Test());
+    }
+}
--- src/com/hspedu/encap/Encapsulation01.java
+++ src/com/hspedu/encap/Encapsulation01.java
@@ -1,37 +1,79 @@
 package com.hspedu.encap;
 
 
-
 public class Encapsulation01 {
     public static void main(String[] args) {
         Person person = new Person();
+        //person.setName("jack wang la");
+        //person.setAge(30);
+        //person.setSalary(200000);
 
+        //System.out.println(person.info());
+        //System.out.println(person.getSalary());
 
+        System.out.println("smith的信息");
+        Person person1 = new Person("smith", 200, 30000);
+        System.out.println(person1.info());
 
-        person.name = "jack";
-        person.setAge(3000);
     }
 }
 
+
+/**
+ * 不能查看年龄 工资  并对设置的年龄进行合理的验证。
+ * 年龄合理就设置  否则默认
+ * 年龄 1-120 年龄 工资不能查看 name的长度在2-6字符之间
+ */
 class Person {
     public String name;
     private int age;
     private double salary;
+
+    //无参构造器
+    public Person() {
+    }
+
+    //有三个属性的构造器
+    public Person(String name, int age, double salary) {
+        //this.name = name;
+        //this.age = age;
+        //this.salary = salary;
+        //将set方法写入构造器
+        this.setName(name);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    //set + get 封装
+    //快捷键  ALT +INSERT
 
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (name.length()>=2 && name.length()<=6){
+            this.name = name;
+        }else {
+            System.out.println("名字长度不对（2-6） 默认");
+            this.name = "文明人";
+        }
     }
 
     public int getAge() {
         return age;
     }
 
+    //年龄必须在1-120
     public void setAge(int age) {
-        this.age = age;
+        if (age >= 1 && age <= 120) {
+            this.age = age;
+        } else {
+            System.out.println("年龄必须在1-120");
+            //默认年龄
+            this.age = 18;
+        }
+
     }
 
     public double getSalary() {
@@ -39,14 +81,15 @@ class Person {
     }
 
     public void setSalary(double salary) {
+        //可以增加当前对象的权限判断
+        //if else return;
         this.salary = salary;
     }
 
-
-    //快捷键  ALT +INSERT
-
-
-
+    //返回属性信息
+    public String info() {
+        return "信息为 name=" + name + " age=" + age + " salry=" + salary;
+    }
 
 
 }
